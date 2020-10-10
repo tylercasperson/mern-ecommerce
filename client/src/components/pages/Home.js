3b289b4d3e6aa917519cb0f05b5399594f4a4184
Tyler Casperson <tylercasperson@gmail.com>
2020-10-09 22:40:46 -0600

    created a product card to display image title rating and price

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
+import Product from '../layout/Product';
 import products from '../../products';
 
 const Home = () => {
@@ -9,7 +10,7 @@ const Home = () => {
       <Row>
         {products.map((product) => (
           <Col sm={12} md={6} lg={4} xl={3}>
-            <h3>{product.name}</h3>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
