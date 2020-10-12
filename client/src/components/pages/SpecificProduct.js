c78205fdd08e66027cdc014f9c9a7713b4ad003e
Tyler Casperson <tylercasperson@gmail.com>
2020-10-11 22:21:34 -0600

    data is being loaded from the backend

--- client/src/components/pages/SpecificProduct.js
+++ client/src/components/pages/SpecificProduct.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../layout/Rating';
-import products from '../../products';
 
 const SpecificProduct = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+
+      setProduct(data);
+    };
+    fetchProduct();
+  }, []);
+
   return (
     <>
       <Link className='btn btn-light my-3' to='/'>
--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.get('/api/products', (req, res) => {
 });
 
 app.get('/api/products/:id', (req, res) => {
-  const product = product.find((p) => p._id === req.params.id);
-  res.json(products);
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
 });
 
 app.listen(5000, console.log('Server running on port 5000'));
