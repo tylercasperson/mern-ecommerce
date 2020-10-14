365eaff1f35c7dfc2e692249c982ec402ccac4cf
Tyler Casperson <tylercasperson@gmail.com>
2020-10-14 00:01:43 -0600

    created an error handler

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import colors from 'colors';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 // import products from './seedData/products.js';
@@ -17,6 +18,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
