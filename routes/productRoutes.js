273cf9b2898636a7c9049252281925104569a2a5
Tyler Casperson <tylercasperson@gmail.com>
2020-10-18 22:21:58 -0600

    user authentication endpoint

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,7 +5,7 @@ import {
   getProductById,
 } from '../controllers/productController.js';
 
-router.get('/').get(getProducts);
-router.get('/:id').get(getProductById);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
