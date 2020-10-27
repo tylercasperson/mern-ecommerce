ce29598e3ec7e50115a59de65d7ac1d46f5863f7
Tyler Casperson <tylercasperson@gmail.com>
2020-10-26 20:21:35 -0600

    added breadcrumbs to the checkout process

--- client/src/components/pages/Shipping.js
+++ client/src/components/pages/Shipping.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../layout/FormContainer';
+import CheckOutSteps from '../layout/CheckOutSteps';
 import { saveShippingAddress } from '../../actions/cartActions';
 
 const Shipping = ({ history }) => {
@@ -23,6 +24,7 @@ const Shipping = ({ history }) => {
 
   return (
     <FormContainer>
+      <CheckOutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId='address'>
