90c4b3e733d2191547a6354a22efcf6133f618cc
Tyler Casperson <tylercasperson@gmail.com>
2020-12-25 18:51:36 -0700

    able to delete users

--- client/src/components/pages/UserList.js
+++ client/src/components/pages/UserList.js
@@ -4,7 +4,7 @@ import { Table, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../../components/layout/Message';
 import Loader from '../layout/Loader';
-import { listUsers } from '../../actions/userActions';
+import { listUsers, deleteUser } from '../../actions/userActions';
 
 const UserList = ({ history }) => {
   const dispatch = useDispatch();
@@ -15,16 +15,21 @@ const UserList = ({ history }) => {
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
+  const userDelete = useSelector((state) => state.userDelete);
+  const { success: successDelete } = userDelete;
+
   useEffect(() => {
     if (userInfo && userInfo.isAdmin) {
       dispatch(listUsers());
     } else {
       history.push('/login');
     }
-  }, [dispatch, history]);
+  }, [dispatch, history, successDelete]);
 
   const deleteHandler = (id) => {
-    console.log('delete');
+    if (window.confirm('Are you sure?')) {
+      dispatch(deleteUser(id));
+    }
   };
 
   return (
