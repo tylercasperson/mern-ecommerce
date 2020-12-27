089b91d091b99fa77fd5ad7a3aa2e6100c03c0f6
Tyler Casperson <tylercasperson@gmail.com>
2020-12-26 22:58:04 -0700

    admins can delete a product

--- client/src/components/pages/ProductList.js
+++ client/src/components/pages/ProductList.js
@@ -4,13 +4,20 @@ import { Table, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../../components/layout/Message';
 import Loader from '../layout/Loader';
-import { listProducts } from '../../actions/productActions';
+import { listProducts, deleteProduct } from '../../actions/productActions';
 
 const ProductList = ({ history, match }) => {
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
+
+  const productDelete = useSelector((state) => state.productDelete);
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = productDelete;
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
@@ -21,11 +28,11 @@ const ProductList = ({ history, match }) => {
     } else {
       history.push('/login');
     }
-  }, [dispatch, history, userInfo]);
+  }, [dispatch, history, userInfo, successDelete]);
 
   const deleteHandler = (id) => {
     if (window.confirm('Are you sure?')) {
-      //   delete products
+      dispatch(deleteProduct(id));
     }
   };
 
@@ -45,6 +52,8 @@ const ProductList = ({ history, match }) => {
           </Button>
         </Col>
       </Row>
+      {loadingDelete && <Loader />}
+      {errorDelete && <Message variant='danger'>error: {errorDelete}</Message>}
       {loading ? (
         <Loader />
       ) : error ? (
