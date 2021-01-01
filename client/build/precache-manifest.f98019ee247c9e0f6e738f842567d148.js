f7bb88f0bdb7c722215ec2244053b9ad3e8c1743
Tyler Casperson <tylercasperson@gmail.com>
2020-12-31 21:02:39 -0700

    preparing to run the production build

---
+++ client/build/precache-manifest.f98019ee247c9e0f6e738f842567d148.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "264d034fc7fcb306777dca67150b59b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e3175d908d9c1913aad",
+    "url": "/static/css/main.64d9efd5.chunk.css"
+  },
+  {
+    "revision": "3e7f628e85e955364b87",
+    "url": "/static/js/2.594ba530.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.594ba530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e3175d908d9c1913aad",
+    "url": "/static/js/main.c747610c.chunk.js"
+  },
+  {
+    "revision": "7f4bae47dd2429304b0b",
+    "url": "/static/js/runtime-main.c5b2c2ae.js"
+  }
+]);
